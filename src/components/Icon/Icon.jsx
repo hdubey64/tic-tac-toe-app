32fbe15b0d1675f4dde1934994d33f36f15266bd
Vishal Dubey <hdubey64@gmail.com>
2023-08-07 17:47:14 +0530

    Icon Component

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+import { FaPen, FaRegCircle, FaTimes } from "react-icons/fa";
+
+import React from "react";
+
+const Icon = ({ name }) => {
+   if (name == "circle") {
+      return <FaRegCircle />;
+   } else if (name == "cross") {
+      return <FaTimes />;
+   } else {
+      return <FaPen />;
+   }
+};
+
+export default Icon;
