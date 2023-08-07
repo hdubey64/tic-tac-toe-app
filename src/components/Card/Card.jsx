a9c412749da37e0f5443cc6bf572e03b853e18f5
Vishal Dubey <hdubey64@gmail.com>
2023-08-07 19:06:58 +0530

    Onplay Function implemented  on Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,18 @@ import Icon from "../Icon/Icon";
 import React from "react";
 import "./Card.css";
 
-const Card = ({ player }) => {
+const Card = ({ player, onPlay, index }) => {
    let icon = <Icon />;
    if (player == "X") {
       icon = <Icon name="cross" />;
    } else if (player == "O") {
       icon = <Icon name="circle" />;
    }
-   return <div className="card">{icon}</div>;
+   return (
+      <div className="card" onClick={() => onPlay(index)}>
+         {icon}
+      </div>
+   );
 };
 
 export default Card;
