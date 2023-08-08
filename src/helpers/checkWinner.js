e552efea1fba951a3ab96c5de0236bc05ebfafb9
Vishal Dubey <hdubey64@gmail.com>
2023-08-08 13:29:27 +0530

    Typpo fixed

--- src/helpers/checkWinner.js
+++ src/helpers/checkWinner.js
@@ -8,9 +8,9 @@ const isWinner = (board, symbol) => {
 
    if (board[0] == board[3] && board[3] == board[6] && board[6] == symbol)
       return symbol;
-   if (board[2] == board[5] && board[5] == board[7] && board[7] == symbol)
+   if (board[1] == board[4] && board[4] == board[7] && board[7] == symbol)
       return symbol;
-   if (board[3] == board[6] && board[6] == board[8] && board[8] == symbol)
+   if (board[2] == board[5] && board[5] == board[8] && board[8] == symbol)
       return symbol;
 
    if (board[0] == board[4] && board[4] == board[8] && board[8] == symbol)
